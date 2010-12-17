e7381cb6ea3d139dfcb599a4f14aa78bec71ed91
David Golden <dagolden@cpan.org>
2010-12-17 08:21:03 -0500

    revise into a Doppelgaenger distribution

---
+++ pod/lib/CPAN/Meta/YAML.pm
@@ -0,0 +1,22 @@
+# ABSTRACT: Read and write a subset of YAML for CPAN Meta files
+
+=head1 DESCRIPTION
+
+This module implements a subset of the YAML specification for use in reading
+and writing CPAN metadata files like F<META.yml> and F<MYMETA.yml>.  It should
+not be used for any other general YAML parsing or generation task.
+
+=head1 SUPPORT
+
+This module is currently derived from L<YAML::Tiny> by Adam Kennedy.  If
+there are bugs in how it parses a particular META.yml file, please file
+a bug report in the YAML::Tiny bugtracker:
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=YAML-Tiny>
+
+=head1 SEE ALSO
+
+L<YAML>, L<YAML::Syck>, L<YAML::LibYAML>, L<YAML::XS>
+
+=cut
+
