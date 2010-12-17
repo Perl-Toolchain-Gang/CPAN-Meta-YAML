eea2759ba0810bea9d761f1ba1056d1b8df9ea19
David Golden <dagolden@cpan.org>
2010-12-17 06:46:19 -0500

    initial import of CPAN-Meta-YAML

---
+++ lib/CPAN/Meta/YAML.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package CPAN::Meta::YAML;
+# ABSTRACT: No abstract given for CPAN::Meta::YAML
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use CPAN::Meta::YAML;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
