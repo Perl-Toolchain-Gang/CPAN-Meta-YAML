eb33a0b1e014be30a8aa92732795964b8b837673
David Golden <dagolden@cpan.org>
2010-12-17 08:36:32 -0500

    tweak RemovePrereqs and SUPPORT pod

--- pod/lib/CPAN/Meta/YAML.pm
+++ pod/lib/CPAN/Meta/YAML.pm
@@ -11,7 +11,6 @@ not be used for any other general YAML parsing or generation task.
 This module is currently derived from L<YAML::Tiny> by Adam Kennedy.  If
 there are bugs in how it parses a particular META.yml file, please file
 a bug report in the YAML::Tiny bugtracker:
-
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=YAML-Tiny>
 
 =head1 SEE ALSO
